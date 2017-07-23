4891e70620c8a7a24809f4082ef0cde1d4b372d1
GoForTheEyes <brabandere@gmail.com>
2017-07-22 22:01:03 -0400

    Saturday edits

---
+++ Assets/Game/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    
+    public int startPlayback = 0;
+    public bool recording = true;
+    public bool started = false;
+
+    // Use this for initialization
+    void Start () {
+        PlayerPrefsManager.UnlockLevel(2);
+        print("Level1: " + PlayerPrefsManager.IsLevelUnlocked(1));
+        print("Level2: " + PlayerPrefsManager.IsLevelUnlocked(2));
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!started && CrossPlatformInputManager.GetAxis("Horizontal") != 0)
+        {
+            started = true;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            startPlayback = Time.frameCount;
+        }
+        if (!CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            recording = true;
+        }
+        else
+        {
+            recording = false;
+        }
+    }
+}
---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem: MonoBehaviour {
+
+    private const int bufferFrames = 1000;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody myRigidbody;
+    private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+        MyKeyFrame testKey = new MyKeyFrame(1f, Vector3.up, Quaternion.identity);
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (gameManager.started)
+        {
+            if (gameManager.recording)
+            {
+                Record();
+            }
+            else
+            {
+                PlayBack(gameManager.startPlayback);
+            }
+        }
+
+    }
+
+    void PlayBack (int startFrame)
+    {
+        myRigidbody.isKinematic = true;
+        int frame = (Time.frameCount - startFrame) % bufferFrames;
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+
+    }
+
+    private void Record()
+    {
+        myRigidbody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+/// <summary>
+/// A structure for storing time, rotation and position.
+/// </summary>
+public struct MyKeyFrame
+{
+    public float time; 
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float newTime, Vector3 newPosition, Quaternion newRotation)
+    {
+        time = newTime;
+        position = newPosition;
+        rotation = newRotation;
+    }
+}
